31ec5f2654a9b3c5c83938c9e6ba3ee0222f5522
danielterziev92 <daniel.st.terziev@gmail.com>
2023-12-22 18:13:06 +0200

    Add FontAwesome to the project

--- src/components/navigation-info-bar/NavigationInfoBar.jsx
+++ src/components/navigation-info-bar/NavigationInfoBar.jsx
@@ -1,10 +1,15 @@
-import style from './NavigationInfoBar.module.css'
 import {useReducer} from "react";
+
+import style from './NavigationInfoBar.module.css'
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const initialState = {
     imageUrl: 'https://img.freepik.com/free-photo/portrait-happy-young-male-network-engineer-with-laptop-hand-working-datacenter_662251-1533.jpg?w=1380&t=st=1703161313~exp=1703161913~hmac=cd7906c7eada3324bcca3df999e12b2e3d74e433e49ca10aca00e312c1bffbc3',
     fullName: 'Daniel Terziev',
     stackPosition: 'Full Stack Python Developer',
+    gitHubUrl: 'https://github.com/danielterziev92/',
+    linkedIdUrl: 'https://www.linkedin.com/in/danielterziev/',
 }
 
 const reducerActions = {
@@ -31,11 +36,20 @@ export default function NavigationInfoBar() {
 
     return (
         <aside className={style.InfoBarFrame}>
-            <figure>
-                <img src={state.imageUrl} alt="Profile picture"/>
-            </figure>
-            <h2>{state.fullName}</h2>
-            <p>{state.stackPosition}</p>
+            <div className={style.InfoBarFrameHeader}>
+                <figure>
+                    <img src={state.imageUrl} alt="Profile picture"/>
+                </figure>
+                <h2>{state.fullName}</h2>
+                <p>{state.stackPosition}</p>
+            </div>
+            <div className={style.InfoBarFrameDetails}>
+
+            </div>
+            <div className={style.InfoBarFrameFooter}>
+                <FontAwesomeIcon icon="fa-brands fa-square-github"/>
+                <FontAwesomeIcon icon="fa-brands fa-linkedin"/>
+            </div>
         </aside>
     );
 }
