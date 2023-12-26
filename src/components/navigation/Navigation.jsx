958122398b49373081055c6ca99c049d4f709e0f
danielterziev92 <daniel.st.terziev@gmail.com>
2023-12-26 23:46:22 +0200

    Create stylization of Navigation component, add useState for activeTab and style it.

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,13 +1,47 @@
+import {useState} from "react";
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 import style from './Navigation.module.css';
+import * as SolidIcons from "@fortawesome/free-solid-svg-icons";
+
+const navigationTabs = [
+    {
+        title: 'Home',
+        iconName: 'faHouse'
+    },
+    {
+        title: 'Contacts',
+        iconName: 'faAddressCard'
+    },
+    {
+        title: 'Blog',
+        iconName: 'faBlog'
+    },
+]
 
 export default function Navigation() {
+    const [activeTabRef, setActiveTabRef] = useState('Home');
+
+    function changeActiveTabClickHandler(e) {
+        setActiveTabRef(e.target.textContent);
+    }
 
     return (
         <nav className={style.navMenu}>
             <ul>
-                <li>Home</li>
-                <li>Contacts</li>
-                <li>Block</li>
+                {navigationTabs.map(({title, iconName}, index) => (
+                    <li key={index}
+                        onClick={changeActiveTabClickHandler}
+                        className={activeTabRef === title ? style.active : ''}
+                    >
+                        <FontAwesomeIcon icon={SolidIcons[iconName]}/>
+                        {title}
+                    </li>
+                ))}
+                {/*<li className={active style.active} onClick={changeActiveTabClickHandler}>Home</li>*/}
+                {/*<li onClick={changeActiveTabClickHandler}>Contacts</li>*/}
+                {/*<li onClick={changeActiveTabClickHandler}>Block</li>*/}
             </ul>
         </nav>
     );
