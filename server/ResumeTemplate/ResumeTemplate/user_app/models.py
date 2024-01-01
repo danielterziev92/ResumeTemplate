e102e7a6fcf7fff3dd0387404ab7d7ea1c7402fc
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-01 21:01:41 +0200

    Create UserProfile model and makemigrations

--- server/ResumeTemplate/ResumeTemplate/user_app/models.py
+++ server/ResumeTemplate/ResumeTemplate/user_app/models.py
@@ -35,3 +35,47 @@ class UserApp(auth_models.AbstractUser, auth_models.PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class UserProfile(models.Model):
+    FIRST_NAME_MAX_LENGTH = 30
+    LAST_NAME_MAX_LENGTH = 30
+    TECH_STACK_MAX_LENGTH = 30
+    PHONE_NUMBER_MAX_LENGTH = 13
+    COUNTRY_NAME_MAX_LENGTH = 30
+
+    user = models.OneToOneField(
+        'UserApp',
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    last_name = models.CharField(
+        max_length=LAST_NAME_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    tech_stack = models.CharField(
+        max_length=TECH_STACK_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    phone_number = models.CharField(
+        max_length=PHONE_NUMBER_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    country_name = models.CharField(
+        max_length=COUNTRY_NAME_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
