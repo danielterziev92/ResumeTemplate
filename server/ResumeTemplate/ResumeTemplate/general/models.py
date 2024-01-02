46014067b62c9d5a50aea5c654274ab14409a253
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-02 21:10:56 +0200

    Create Ability model

--- server/ResumeTemplate/ResumeTemplate/general/models.py
+++ server/ResumeTemplate/ResumeTemplate/general/models.py
@@ -16,3 +16,20 @@ class Skill(models.Model):
         null=False,
         blank=False,
     )
+
+
+class Ability(models.Model):
+    NAME_MAX_LENGTH = 50
+
+    name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    skills = models.ManyToManyField(
+        Skill,
+        related_name='ability_skills',
+        null=False,
+        blank=False,
+    )
