7a24a9e95442863da6b1b1f5640a967a2f8c131f
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-02 20:17:35 +0200

    Create skill model

--- server/ResumeTemplate/ResumeTemplate/general/models.py
+++ server/ResumeTemplate/ResumeTemplate/general/models.py
@@ -6,3 +6,13 @@ class BannerText(models.Model):
         null=False,
         blank=False
     )
+
+
+class Skill(models.Model):
+    NAME_MAX_LENGTH = 50
+
+    name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
